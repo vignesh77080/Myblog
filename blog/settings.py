ea0961f7302dc65b80c5b905cb9aadbccc1d73ed
vignesh77080 <vignesh.yes97@gmail.com>
2020-01-01 22:45:24 +0530

    Up DB

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,5 +133,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
 MEDIA_ROOT = os.path.join( BASE_DIR , 'media' ) 
 MEDIA_URL = '/media/' 
+
+django_heroku.settings(locals())
